1e326fd11f25736b68f3de12f81fe50cd5f764d6
Kelvin Lee <kelvinlee@Kelvins-MacBook-Pro.local>
2020-12-15 05:19:07 +0800

    adsf

--- vars/cfhBuild.groovy
+++ vars/cfhBuild.groovy
@@ -10,7 +10,7 @@ def buildAngular(String tempName, String repo) {
     // $ docker build --rm --target BUILD -t channel/mobileweb:test . && docker build --rm -t channel/mobileweb .
 }
 
-def testAngular() {
+def testAngular(String tempName) {
     // echo "execute tests using docker container..."
     // // sh 'docker run -t ${env.REPO_NAME}:test npm run testCI'
     // sh 'mkdir -p coverage && \
@@ -18,5 +18,5 @@ def testAngular() {
     //     ${env.REPO_NAME}:test npm run testCI'
     // cobertura coberturaReportFile: 'coverage/cobertura-coverage.xml'
     echo "KL-10/15: tests disabled until Matthew/TP fixes their test-cases"
-    sh "docker rmi ${env.REPO_NAME}:test"
+    sh "docker rmi ${tempName}:test"
 }
