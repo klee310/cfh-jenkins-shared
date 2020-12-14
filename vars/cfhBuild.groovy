f67702dbeb4bc8e0cadb3fe2cec3cf6965310690
Kelvin Lee <kelvinlee@Kelvins-MacBook-Pro.local>
2020-12-14 15:41:55 +0800

    asdf

--- vars/cfhBuild.groovy
+++ vars/cfhBuild.groovy
@@ -1,3 +1,15 @@
 def call(map config) {
 
 }
+
+def buildAngular(String tempName, String repo) {
+    echo "REPO_NAME ~~ ${tempName}"
+    echo "REPO ~~ ${repo}"
+    // sh "docker build --rm --target BUILD -t ${tempName}:test . && \
+    //     docker build --rm -t ${repo}:latest ."
+    // $ docker build --rm --target BUILD -t channel/mobileweb:test . && docker build --rm -t channel/mobileweb .
+}
+
+def testAngular() {
+
+}
