aed960b17bef9f49292af9d3ee6b697c04925e82
Kelvin Lee <kelvinlee@Kelvins-MacBook-Pro.local>
2020-12-15 06:39:57 +0800

    asdf

--- vars/cfhPush.groovy
+++ vars/cfhPush.groovy
@@ -1,12 +1,12 @@
 def call() {
     docker.withRegistry("${env.REPO_URL}", "${env.REPO_CRED}") {
         // sh "aws ecr create-repository --repository-name ${env.REPO_NAME} --image-scanning-configuration scanOnPush=true --region ${env.ECR_REGION} || true"
-        // sh "docker tag ${env.REPO}:latest ${env.REPO}:${env.BUILD_TAG} && \
-        //     docker push ${env.REPO}:latest && \
-        //     docker push ${env.REPO}:${env.BUILD_TAG}"
-        echo 'sh "docker tag ${env.REPO}:latest ${env.REPO}:${env.BUILD_TAG} && \
+        sh "docker tag ${env.REPO}:latest ${env.REPO}:${env.BUILD_TAG} && \
             docker push ${env.REPO}:latest && \
-            docker push ${env.REPO}:${env.BUILD_TAG}"'
+            docker push ${env.REPO}:${env.BUILD_TAG}"
+        // echo 'sh "docker tag ${env.REPO}:latest ${env.REPO}:${env.BUILD_TAG} && \
+        //     docker push ${env.REPO}:latest && \
+        //     docker push ${env.REPO}:${env.BUILD_TAG}"'
     }
     sh "docker rmi ${env.REPO}:${env.BUILD_TAG}"
 }
