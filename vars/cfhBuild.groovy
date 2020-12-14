f4e2304925e304fad01da87dbc230d7fe47c2912
Kelvin Lee <kelvinlee@Kelvins-MacBook-Pro.local>
2020-12-15 05:38:01 +0800

    asdf

--- vars/cfhBuild.groovy
+++ vars/cfhBuild.groovy
@@ -2,10 +2,8 @@
 // }
 
 def buildAngular(String testRepoName, String repo) {
-    echo "REPO_NAME ~~ ${testRepoName}"
-    echo "REPO ~~ ${repo}"
-    // sh "docker build --rm --target BUILD -t ${testRepoName}:test . && \
-    //     docker build --rm -t ${repo}:latest ."
+    sh "docker build --rm --target BUILD -t ${testRepoName}:test . && \
+        docker build --rm -t ${repo}:latest ."
     // $ docker build --rm --target BUILD -t channel/mobileweb:test . && docker build --rm -t channel/mobileweb .
 }
 
@@ -16,6 +14,5 @@ def testAngular(String testRepoName) {
     //     docker run -t --mount type=bind,source=$PWD/coverage,target=/usr/src/app/coverage \
     //     ${env.REPO_NAME}:test npm run testCI'
     // cobertura coberturaReportFile: 'coverage/cobertura-coverage.xml'
-    echo "KL-10/15: tests disabled until Matthew/TP fixes their test-cases"
     sh "docker rmi ${testRepoName}:test"
 }
