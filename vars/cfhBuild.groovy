2db1ebac177efb587b42543b45352dc4084c8b3c
Kelvin Lee <kelvinlee@Kelvins-MacBook-Pro.local>
2020-12-17 15:06:07 +0800

    asdf

--- vars/cfhBuild.groovy
+++ vars/cfhBuild.groovy
@@ -1,21 +1,22 @@
 // def call(map config) {
 // }
 
-def buildAngular(String testRepoName, String repo) {
+def buildAngular() {
     echo "Build Docker Image..."
-    sh "docker build --rm --target BUILD -t ${testRepoName}:test . && \
-        docker build --rm -t ${repo}:latest ."
+    sh "docker build --rm --target BUILD -t tempimage:test . && \
+        docker build --rm -t ${env.REPO}:latest ."
     // $ docker build --rm --target BUILD -t channel/mobileweb:test . && docker build --rm -t channel/mobileweb .
 }
 
-def testAngular(String testRepoName) {
+def testAngular() {
     // echo "execute tests using docker container..."
     // // sh 'docker run -t ${env.REPO_NAME}:test npm run testCI'
     // sh 'mkdir -p coverage && \
     //     docker run -t --mount type=bind,source=$PWD/coverage,target=/usr/src/app/coverage \
     //     ${env.REPO_NAME}:test npm run testCI'
     // cobertura coberturaReportFile: 'coverage/cobertura-coverage.xml'
-    sh "docker rmi ${testRepoName}:test"
+    echo "KL-10/15: tests disabled until Matthew/TP fixes their test-cases"
+    sh "docker rmi tempimage:test"
 }
 
 def buildSpring() {
