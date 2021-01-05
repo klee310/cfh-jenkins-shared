665f8fba64fa378668a2293f46e7afa5518f08a3
Kelvin Lee <kelvinlee@cathayholdings.com.tw>
2021-01-05 16:18:53 +0800

    push additional tag for "branch"-latest

--- vars/cfhPush.groovy
+++ vars/cfhPush.groovy
@@ -3,11 +3,10 @@ def call() {
     docker.withRegistry("${env.REPO_URL}", "${env.REPO_CRED}") {
         // sh "aws ecr create-repository --repository-name ${env.REPO_NAME} --image-scanning-configuration scanOnPush=true --region ${env.ECR_REGION} || true"
         sh "docker tag ${env.REPO}:latest ${env.REPO}:${env.BUILD_TAG} && \
+            docker tag ${env.REPO}:latest ${env.REPO}:${env.BRANCH_NAME}-latest && \
             docker push ${env.REPO}:latest && \
-            docker push ${env.REPO}:${env.BUILD_TAG}"
-        // echo 'sh "docker tag ${env.REPO}:latest ${env.REPO}:${env.BUILD_TAG} && \
-        //     docker push ${env.REPO}:latest && \
-        //     docker push ${env.REPO}:${env.BUILD_TAG}"'
+            docker push ${env.REPO}:${env.BUILD_TAG} && \
+            docker push ${env.REPO}:${env.BRANCH_NAME}-latest"
     }
     sh "docker rmi ${env.REPO}:${env.BUILD_TAG}"
 }
