00b7a4595d97e45efb5e5cad08cd0ee8a412ee9e
Kelvin Lee <kelvinlee@cathayholdings.com.tw>
2021-01-14 19:19:38 +0800

    testing new notify method

--- vars/cfhNotify.groovy
+++ vars/cfhNotify.groovy
@@ -1,16 +1,14 @@
 def call() {
-    // notifyLINE("IYinezD7zpHt3DbtIKw67n1XCCgskFcVilzGE3nddhW", currentBuild.currentResult == 'SUCCESS')
-
     withCredentials([string(credentialsId: 'acd8aa7c-57a3-47a1-997b-62ba4a7e1571', variable: 'LINE_TOKEN')]) {
         notifyLINE("${LINE_TOKEN}", currentBuild.currentResult == 'SUCCESS')
     }
 }
 
 def notifyLINE(token, isSuccess) {
-    // curl https://notify-api.line.me/api/notify -H 'Authorization: Bearer IYinezD7zpHt3DbtIKw67n1XCCgskFcVilzGE3nddhW' -F 'message=test 🎉 🎄 👻' -F 'stickerPackageId=2' -F 'stickerId=28'"
+    // backup...🎉 🎄 👻 🐂 🐀
 
     def url = 'https://notify-api.line.me/api/notify'
-    def msg_prefix = isSuccess ? "🎉 *${currentBuild.currentResult}* 🎉" : "👻 *${currentBuild.currentResult}* 👻"
+    def msg_prefix = isSuccess ? "🐂*${currentBuild.currentResult}*🐂" : "🐀*${currentBuild.currentResult}*🐀"
     // def message = "Build ```${env.REPO_NAME}:${env.BUILD_TAG}``` *${currentBuild.currentResult}*\n${env.BUILD_URL}"
     def message = msg_prefix + "\n```${env.REPO_NAME}:${env.BUILD_TAG}```\n${env.BUILD_URL}"
     // def stickerPackageId = '2'
@@ -18,4 +16,12 @@ def notifyLINE(token, isSuccess) {
 
     // sh "curl ${url} -H 'Authorization: Bearer ${token}' -F 'message=${message}' -F 'stickerPackageId=${stickerPackageId}' -F 'stickerId=${stickerId}'"
     sh "curl ${url} -H 'Authorization: Bearer ${token}' -F 'message=${message}'"
+}
+
+def notifyLine2(config) {
+    withCredentials([string(credentialsId: 'acd8aa7c-57a3-47a1-997b-62ba4a7e1571', variable: 'LINE_TOKEN')]) {
+        if (config.message) {
+            sh "curl https://notify-api.line.me/api/notify -H 'Authorization: Bearer ${LINE_TOKEN}' -F 'message=${config.message}'"
+        }
+    }
 }
